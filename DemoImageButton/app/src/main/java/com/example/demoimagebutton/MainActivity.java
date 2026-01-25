b0ce026d96bca910344be80218bb8af8bcc4e971
Sang <tansang06092004@gmail.com>
2026-01-26 01:00:02 +0700

    hi

--- DemoImageButton/app/src/main/java/com/example/demoimagebutton/MainActivity.java
+++ DemoImageButton/app/src/main/java/com/example/demoimagebutton/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.demoimagebutton;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imageView;
+    Button buttonFb, buttonTt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        imageView = findViewById(R.id.imageView);
+    }
+
+    public void showFace(View view) {
+        imageView.setImageResource(R.drawable.facebook);
+    }
+
+    public void showTiktok(View view) {
+        imageView.setImageResource(R.drawable.tiktok);
     }
 }
